afa54f42ff719cfc4bab8799191944cb8f3045c2
wooningeire <22846982+wooningeire@users.noreply.github.com>
2024-03-26 21:14:17 -0400

    feat: add condgen

--- src/routes/web/+page.svelte
+++ src/routes/web/+page.svelte
@@ -6,6 +6,7 @@ import WebGalleryEntry from "$/components/WebGalleryEntry.svelte";
 
 import r4itumPreviewSrc from "$/assets/web-screenshots/r4itum.png";
 import colorspacesPreviewSrc from "$/assets/web-screenshots/colorspaces.png";
+import condgen3PreviewSrc from "$/assets/web-screenshots/condgen3.png";
 import soloyyinProtoPreviewSrc from "$/assets/web-screenshots/soloyyin.proto.png";
 import familyEmojiBuilderPreviewSrc from "$/assets/web-screenshots/family-emoji-builder.png";
 import r4itum2020PreviewSrc from "$/assets/web-screenshots/r4itum.2020.png";
@@ -27,6 +28,11 @@ onMount(() => {
         preview={colorspacesPreviewSrc}
         title="Color space explorer"
         url="/colorspaces"
+    />
+    <WebGalleryEntry
+        preview={condgen3PreviewSrc}
+        title="Conditional generator"
+        url="/condgen3"
     />
     <WebGalleryEntry
         preview={soloyyinProtoPreviewSrc}
